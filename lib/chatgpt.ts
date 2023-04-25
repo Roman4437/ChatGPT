81814dcad1e83cbd5d672edcfed18f1a2ea7ae69
Roman4437 <romanbabalici97@gmail.com>
2023-04-25 18:24:27 +0200

    remove user-agent header from openaiapi request

--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -4,6 +4,8 @@ const configuration = new Configuration({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY
 })
 
+delete configuration.baseOptions.headers['User-Agent']
+
 const openai = new OpenAIApi(configuration)
 
 export default openai
